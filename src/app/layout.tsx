2e6cb0b1b97328d42cdbddef604410d6d4183cfe
Andressa Soares <andressa.soares@pg.intra>
2026-04-10 17:43:44 -0300

    feat: add Google Ads gtag.js tracking tag (AW-17861688846)

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,6 @@
 import type { Metadata, Viewport } from 'next'
 import { Inter } from 'next/font/google'
+import Script from 'next/script'
 import './globals.css'
 import { AppShell } from '@/components/layout/AppShell'
 import { PageViewTracker } from '@/components/tracking/PageViewTracker'
@@ -58,6 +59,20 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="pt-BR" className={`${inter.variable}`}>
+      <head>
+        <Script
+          src="https://www.googletagmanager.com/gtag/js?id=AW-17861688846"
+          strategy="afterInteractive"
+        />
+        <Script id="google-ads-gtag" strategy="afterInteractive">
+          {`
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', 'AW-17861688846');
+          `}
+        </Script>
+      </head>
       <body className="min-h-screen flex flex-col antialiased">
         <PageViewTracker />
         <AppShell>{children}</AppShell>
